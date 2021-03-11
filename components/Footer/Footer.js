c51a107b776c2f24c4ceb80adea260c10d2de0b1
burrodecarga <edwinedwinhenriquezh@hotmail.com>
2021-03-11 12:37:44 -0400

    error capturado

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -12,13 +12,10 @@ import styles from '../Footer/Footer.module.css'
 export default function Footer() {
   return (
     <>
-      <footer className={styles.footer}>
+      <footer className={styles.Logofooter}>
         <div className={styles.logoFooter}>
-<<<<<<< HEAD
           <img className={styles.img} src="/assets/media/logo-white.png" alt="company logo" />
-=======
           <img className={styles.img} src="/assets/media/logowhite.png" alt="company logo"  />
->>>>>>> ac35017673e1dbe27411056564d40acd7071c1f3
         </div>
         <div className={styles.txt}>
           <h3 className={styles.h3}>for candidates</h3>
